a45f641432706b4bb5f6ba5581d2a5407f06ead5
Rohit Singh <info@bitemate.com.au>
2025-11-29 20:07:35 +1100

    Intial Commit With Solution & Folder setup for ApiRefactor.Solution

---
+++ ApiRefactor.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace ApiRefactor.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
